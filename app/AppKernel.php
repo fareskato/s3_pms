65494b6033ec4028ce0d3df556944e0a470becd1
fares kato <fareskato@gmail.com>
2017-09-18 05:31:33 +0300

    Fos user bundle, Liip_imagine bundle, and Twig extension bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,10 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            // FOS user bundle
+            new FOS\UserBundle\FOSUserBundle(),
+            // Liip_imagine bundle
+            new Liip\ImagineBundle\LiipImagineBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
