a45c45867b50af7150ac411c318add0f77f0a18f
fares kato <fareskato@gmail.com>
2017-09-18 04:48:53 +0300

    Admin Panel theme implemented

--- src/AppBundle/Controller/Admin/AdminController.php
+++ src/AppBundle/Controller/Admin/AdminController.php
@@ -1,19 +1,19 @@
 <?php
 
-namespace AppBundle\Controller;
+namespace AppBundle\Controller\Admin;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+class AdminController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/admin", name="dashboard")
      */
     public function indexAction()
     {
         // replace this example code with whatever you need
-        return $this->render('base.html.twig');
+        return $this->render('admin/dashboard.html.twig');
     }
 }
